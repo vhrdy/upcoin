c150bea97e800c94003ed322f32f11352a8b6556
Valentin <hardy.valpro@gmail.com>
2025-11-06 23:44:03 +0100

    update seo

--- src/components/Terminal.tsx
+++ src/components/Terminal.tsx
@@ -5,16 +5,21 @@ import {
   } from '@/components/ui/shadcn-io/terminal';
   const Example = () => (
     <Terminal className="w-full max-w-full">
-      <AnimatedSpan delay={0}>$ npm install upcoin</AnimatedSpan>
+      <AnimatedSpan delay={100}>$ npm install upcoin</AnimatedSpan>
       <TypingAnimation delay={1000} duration={100}>
         Installing dependencies...
       </TypingAnimation>
-      <AnimatedSpan delay={3000}>✓ Package installed successfully</AnimatedSpan>
-      <AnimatedSpan delay={3500}>$ npm run dev</AnimatedSpan>
-      <TypingAnimation delay={4500} duration={80}>
+      <AnimatedSpan delay={4000}>✓ Package installed successfully</AnimatedSpan>
+      <AnimatedSpan delay={6500}>$ npm run dev</AnimatedSpan>
+      <TypingAnimation delay={8500} duration={80}>
         Starting going up only...
       </TypingAnimation>
-      <AnimatedSpan delay={6500}>🚀 Server ready at https://upcoin.lol</AnimatedSpan>
+      <AnimatedSpan delay={10500}>🚀 Server ready at https://upcoin.lol</AnimatedSpan>
+      <TypingAnimation delay={12500} duration={80}>
+      ✓ Ready to buy at...
+      </TypingAnimation>
+      <AnimatedSpan delay={14500}>CgwW9y8aLVJKGSLbjRr23mY1iGSDpwgbQetLAph15fRk</AnimatedSpan>
+
     </Terminal>
   );
   export default Example;
